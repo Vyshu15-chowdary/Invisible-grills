bd47ee6fedfaf7fcee1fcd9996c8aac3b0559782
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-08 18:42:37 +0530

    App Updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
+
+      <header>
+      <h1>Invisible Grills</h1>
+      </header>
+
+      <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatibus exercitationem excepturi aperiam dolorum voluptates voluptatum laudantium itaque iste velit.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, quia amet tempore repellendus dignissimos ex laborum? Totam maiores animi maxime ratione, in explicabo aliquid obcaecati!</p>
+      </main>
+
+      <footer>
+        <p>Invisible Grills @2025</p>
+      </footer>
     </>
   )
 }
