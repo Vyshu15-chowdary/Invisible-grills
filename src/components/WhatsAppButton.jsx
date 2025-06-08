7adb6b54f6d5d3532f4c2d1138a38be776556e92
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-08 23:57:58 +0530

    WhatsApp Button Styled And Placed.

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import wIcon from '../assets/WhatsApp.svg.webp'
+import '../css/whatsappButton.css'
+
 const WhatsAppButton = () => {
 
     const message = `Hi, I'm looking for a free consultation for invisible grills in Nellore. Could you schedule a site visit and provide a quotation?`;
@@ -13,7 +15,7 @@ const WhatsAppButton = () => {
 
     return (
         <>
-        <img src={wIcon} alt="whatsapp icon" onClick={handleClick} />
+        <img className='wIcon' src={wIcon} alt="whatsapp icon" onClick={handleClick} />
         </>
     )
 }
