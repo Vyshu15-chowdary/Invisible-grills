c8094ba2bcd067387c14eadd79856bc2633c3f5b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-08 22:56:09 +0530

    Page Routing Completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Gallery from './pages/Gallery'
+import Services from "./pages/Services"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 function App() {
 
 
   return (
     <>
-     
-
-      <header>
-      <h1>Invisible Grills</h1>
-      </header>
-
-      <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatibus exercitationem excepturi aperiam dolorum voluptates voluptatum laudantium itaque iste velit.</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, quia amet tempore repellendus dignissimos ex laborum? Totam maiores animi maxime ratione, in explicabo aliquid obcaecati!</p>
-      </main>
-
-      <footer>
-        <p>Invisible Grills @2025</p>
-      </footer>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/services' element={<Services/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/gallery' element={<Gallery/>}/>
+      
+     </Routes>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+       <header>
+      <h1>Invisible Grills</h1>
+      </header>
+
+      <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatibus exercitationem excepturi aperiam dolorum voluptates voluptatum laudantium itaque iste velit.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, quia amet tempore repellendus dignissimos ex laborum? Totam maiores animi maxime ratione, in explicabo aliquid obcaecati!</p>
+      </main>
+
+      <footer>
+        <p>Invisible Grills @2025</p>
+      </footer>
+    </>
   )
 }
 
