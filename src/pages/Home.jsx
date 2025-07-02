d586725d690a6010b649a8a2c047038975b5044e
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-02 23:12:46 +0530

    GoogleMap Placed In Contact Page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import GoogleMap from '../components/GoogleMap';
+
 
 const Home = () => {
   const ImgUrl = 'https://www.rohitaluminium.com/wp-content/uploads/2023/03/WhatsApp-Image-2023-02-14-at-3.44.44-PM.jpeg';
   const heroText = 'Welcome To Home Page.';
+
+
   return (
     <>
       <Hero heading={heroText} ImgUrl={ImgUrl} />
-      <GoogleMap/>
+
     </>
   )
 }
