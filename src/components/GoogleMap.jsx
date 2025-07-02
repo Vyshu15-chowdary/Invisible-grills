3a4c2143cb7dc207ffc632350398b334135b9147
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-02 23:01:31 +0530

    Golcunda Fort Location Added For Testing Purpuse.

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const GoogleMap = () => {
+    const mapStyle = {
+        width: '100%',
+        height: '450px',
+        border: '0',
+    };
   return (
     <div>
-    Google Map Component
+    <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d60921.46136955282!2d78.3598523338936!3d17.38338678938934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x3bcb968ea6aea98f%3A0x6b76ecdfd65a335a!2sGolconda%20Fort%2C%20Khair%20Complex%2C%20Ibrahim%20Bagh%2C%20Hyderabad%2C%20Telangana%20500008!3m2!1d17.383309099999998!2d78.4010522!5e0!3m2!1sen!2sin!4v1751477379718!5m2!1sen!2sin" style={mapStyle} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   )
 }
