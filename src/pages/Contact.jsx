a13e4241168e88114fc0c1b077f90601d725d940
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-03 21:16:37 +0530

    Hero Component Added In Contact Page.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import GoogleMap from '../components/GoogleMap'
+import Hero from '../components/Hero';
 
 const Contact = () => {
+  const ImgUrl = "https://www.invisiblegrillmanufacturer.com/wp-content/uploads/2025/03/Balaji-Enterprises-in-Pune-invisible-grills-manufacturers-1024x768.jpg";
+
+  const heading = "Contact Us";
+  
   return (
     <>
+    <Hero ImgUrl={ImgUrl} heading={heading}/>
     <GoogleMap/>
     </>
   )
