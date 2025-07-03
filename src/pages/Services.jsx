978a3cba50c61b525c15d0b19263df9ffdbf0a44
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-03 21:57:49 +0530

    Service Page Hero Section Added.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Hero from '../components/Hero';
 
 const Services = () => {
+  const ImgUrl = "https://cdn-ikpjmaj.nitrocdn.com/tpiOjaVkKiYlOPaZEGMJBhdGjCOUFngB/assets/images/optimized/rev-79ca70e/elelaurels.com/wp-content/uploads/2016/12/InvisGrilleBalcony2.jpg";
+
+  const heading = "Our Services";
   return (
-    <div>Services</div>
+    <>
+    <Hero  ImgUrl={ImgUrl} heading={heading}  />
+    </>
   )
 }
 
