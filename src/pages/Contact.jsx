3d5f830e1f74a44dec4c5407f6ccd121fd6ba014
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-03 22:51:41 +0530

    Contact Form Added In Contact Page.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import GoogleMap from '../components/GoogleMap'
 import Hero from '../components/Hero';
+import ContactForm from '../components/ContactForm.jsx';
 
 const Contact = () => {
   const ImgUrl = "https://hangerswala.com/wp-content/uploads/2025/05/assets_task_01jv1v3z1dfjs8c6zszwdbcdsh_1747040025_img_0.webp";
@@ -10,6 +11,8 @@ const Contact = () => {
   return (
     <>
     <Hero ImgUrl={ImgUrl} heading={heading}/>
+ 
+    <ContactForm/>
     <GoogleMap/>
     </>
   )
