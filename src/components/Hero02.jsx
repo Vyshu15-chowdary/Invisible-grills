ede8a4795faddce884f88c5ec08dda25c40f6e4f
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-03 20:16:41 +0530

    Hero02 Component Styled.

--- src/components/Hero02.jsx
+++ src/components/Hero02.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import "../css/hero02.css"
 
-const Hero02 = () => {
+const Hero02 = ({heading, ImgUrl}) => {
+
+    const hero02Style = {
+         backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.21), rgba(0, 0, 0, 0.356)), url(${ImgUrl})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+    }
+
   return (
-    <div>Hero02</div>
+    <div className='hero02' style={hero02Style}>
+        <h1>{heading}</h1>
+    </div>
   )
 }
 
