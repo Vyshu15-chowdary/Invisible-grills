1f7527cbbdd51c759a136b6c95ed5347e9254703
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-04 23:53:38 +0530

    QuickServiceRequest Styled.

--- src/components/QuickServiceRequest.jsx
+++ src/components/QuickServiceRequest.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
+import '../css/quickServiceRequest.css';
+
+
 
 const QuickServiceRequest = () => {
   return (
     <div className='qsr'>
-      <div className="wrapper">
+      <div className="txt-wrapper">
         <h3>Quick Service Request</h3>
         <h2>Free Inspection Request</h2>
       </div>
-      <div className="wrapper">
+     
         <form action="">
-          <div className="input-box">
+          
             <input type="text" name='name' placeholder='Enter Full Name' />
-            <p></p>
-          </div>
-          <div className="input-box">
             <input type="text" name='phone' placeholder='Enter Phone Number' />
-            <p></p>
-          </div>
-
-          <div className="btn-box">
+       
             <button>Send</button>
-          </div>
+        
         </form>
-      </div>
+     
     </div>
   )
 }
