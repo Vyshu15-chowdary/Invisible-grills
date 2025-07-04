a28520afba8dc4bf7bc0a6a490134a14f7a0562a
vyshnavi <vishuchowdary20@gmail.com>
2025-07-04 19:21:31 +0530

    header1 written

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,10 +4,23 @@ import Hero from '../components/Hero';
 const Services = () => {
   const ImgUrl = "https://cdn-ikpjmaj.nitrocdn.com/tpiOjaVkKiYlOPaZEGMJBhdGjCOUFngB/assets/images/optimized/rev-79ca70e/elelaurels.com/wp-content/uploads/2016/12/InvisGrilleBalcony2.jpg";
 
-  const heading = "Our Services";
+  const heading = "Your Safety, Our Priority – Invisible Grills with a View";
   return (
     <>
     <Hero  ImgUrl={ImgUrl} heading={heading}  />
+
+    <div className='services'>
+      <div className='header'>
+        <h2>Premium Invisible Grills Installation & Customization</h2>
+         <p>At Vyshnavi Invisible Grills, we specialize in offering top-quality invisible grill solutions designed to enhance safety without compromising style or view.
+           Whether for homes,
+           high-rise apartments, offices, or commercial spaces — our grills blend modern aesthetics with the highest safety standards.
+
+</p>
+      </div>
+
+
+    </div>
     </>
   )
 }
