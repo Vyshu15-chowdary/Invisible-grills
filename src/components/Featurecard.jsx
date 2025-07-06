9d0db7ec2642352da01d66b7b16ba64a21a1bcc8
vyshnavi <vishuchowdary20@gmail.com>
2025-07-06 13:49:55 +0530

    created css of featurecard

--- src/components/Featurecard.jsx
+++ src/components/Featurecard.jsx
@@ -11,5 +11,7 @@ export const Featurecard = () => {
   )
 }
 
+export default Featurecard;
+
 
 
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import '../css/Services.css'
-import ImageSlider from '../components/Featurecard';
+import Featurecard from '../components/Featurecard';
+
 
 
 const Services =[
@@ -70,7 +71,9 @@ return(
     </div>
   </section>
 
-    <ImageSlider/>
+  <Featurecard/>
+
+    
 
   </>
 )
