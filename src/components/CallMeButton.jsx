86cc6b2d6d4dc8148bb38c25fee8adbaabbb9fab
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-06 20:29:38 +0530

    CallButton Functionality Updated.

--- src/components/CallMeButton.jsx
+++ src/components/CallMeButton.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import pIcon from '../assets/Phone.webp';
+import useWindowWidth from '../hooks/useWindowWidth'
+
 import "../css/phoneButton.css"
 const CallMeButton = () => {
     const phoneNumber = '+919052585999';
+
+    const width = useWindowWidth();
+    const isPhone = width < 768;
+
   return (
     <>
-    <a href={`tel:${phoneNumber}`}>
+    {
+      isPhone? <a href={`tel:${phoneNumber}`}>
         <img className='pIcon' src={pIcon} alt="call me icon" />
-    </a>
+    </a> : null
+    }
+   
     </>
   )
 }
