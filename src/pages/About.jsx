f1e5a05e959a96188630b47411c8bcfc5d9b6a91
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-07 23:50:12 +0530

    About Page Completely Updated.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,15 +9,11 @@ const About = () => {
   return (
     <>
     <Hero heading={heading} ImgUrl={ImgUrl}/>
-      <section className='About'>
-        <div className='content'>
-          <div className='title'>
-            <h1>welcome </h1>
-            <h1>To</h1>
-            <h1> <span>VIG</span> invisible <span> Grills</span></h1>
-          </div>
-          <div className='sub-title'>
-            <p>At Vyshnavi Invisible Grills, we redefine safety with elegance.
+
+    <div className="about-us">
+      <div className="about-content">
+        <h2>Why Us?</h2>
+        <p>At Vyshnavi Invisible Grills, we redefine safety with elegance.
               Specializing in high-quality invisible grill installations,
               we offer modern, secure, and stylish solutions for homes,
               apartments, balconies, windows, and commercial spaces.</p>
@@ -26,36 +22,9 @@ const About = () => {
               With a commitment to excellence and customer satisfaction,
               we tailor every project to your needs, providing sleek protection for your loved ones—especially
               children and pets—while enhancing the aesthetics of your living space.</p>
-
-          </div>
-        </div>
-      </section>
-
-
-      <div id='flexcontainer'>
-        <div id='cards'>
-          <div className='card'>
-            <img src="./project.jpg" alt="projects completed"></img>
-            <h2>500+</h2>
-            <p>PROJECTS DONE</p>
-          </div>
-        </div>
-
-        <div className='card'>
-          <img src="./clients.jpg" alt="customers"></img>
-          <h2>500+</h2>
-          <p>CLIENTS SATISFIED</p>
-
-        </div>
-        <div className='card'>
-          <img src="./quality.jpg" alt="customers"></img>
-          <h2> quality Material </h2>
-          <p>NUMBER 1 QUALITY</p>
-
-        </div>
-
       </div>
-
+    </div>
+      
     </>
 
 
