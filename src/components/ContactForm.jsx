482492ee8fa73ea020e5cb7c9ec914f511295973
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-07 22:48:17 +0530

    ContactForm Styled.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react'
+import img from "../assets/contactUs.png"
+
 // import emailjs from '@emailjs/browser';
 
+import "../css/contactForm.css"
 
 
 const ContactForm = () => {
@@ -22,26 +25,44 @@ const ContactForm = () => {
     // }
     return (
         <>
-            <h2>Contact Us</h2>
-            <form ref={form} >
+            <div className="contact-form">
 
-                <div className="form-control">
-                    <input type="text" name='name'  placeholder='Enter Your Name' />
-                </div>
-                <div className="form-control">
-                    <input type="text" name='email' placeholder='Enter Your Email ' />
-                </div>
-                <div className="form-control">
-                    <input type="text" name='phone' placeholder='Enter Your Phone Number ' />
-                </div>
-                <div className="form-control">
-                   <textarea name="message" id="" placeholder='Write Your Message Here..'></textarea>
-                </div>
 
-                <div className="btn-control">
-                    <button>Submit</button>
+                <div className="contact-form-content">
+                    <div className="contact-form-img">
+                        <img src={img} alt="contact form" />
+                    </div>
+
+                    <form ref={form} >
+                        <div className="contact-form-header">
+                            <h2>Get In Touch</h2>
+    <hr />
+
+                        </div>
+
+                        <div className="form-features">
+                            <div className="form-control">
+                                <input type="text" autoComplete='off' name='name' placeholder='Enter Your Name' />
+                            </div>
+                            <div className="form-control">
+                                <input type="text" autoComplete='off' name='email' placeholder='Enter Your Email ' />
+                            </div>
+                            <div className="form-control">
+                                <input type="text" autoComplete='off' name='phone' placeholder='Enter Your Phone Number ' />
+                            </div>
+                            <div className="form-control">
+                                <textarea name="message" rows={4} id="" placeholder='Write Your Message Here..'></textarea>
+                            </div>
+
+                            <div className="btn-control">
+                                <button>Submit</button>
+                            </div>
+                        </div>
+
+                    </form>
                 </div>
-            </form>
+            </div>
+
         </>
     )
 }
