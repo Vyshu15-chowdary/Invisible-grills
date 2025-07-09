b8e722044247a65498b4dbe44dee57544c9a53fd
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-09 22:53:23 +0530

    Gallery Page Updated.

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -5,121 +5,104 @@ import '../css/Gallery.css';
 
 
 
-  const heading = "Gallery";
-  const images = [
-        './CH1.jpg',
-        './CH2.webp',
-        
-        './CH4.webp',
-        
-        './CH6.webp',
-        './CH7.jpg',
-        './CH8.jpg'
-        
-  ];
+const images = [
+  './CH1.jpg',
+  './CH2.webp',
+  './CH4.webp',
+  './CH6.webp',
+  './CH7.jpg',
+  './CH8.jpg'
 
-  const MeshImages = [
+];
 
-    './MD1.jpg',
-    './MD2.webp',
-    './MD3.jpg',
-    './MD4.jpg',
-    './MD5.jpeg',
-    './MD6.webp'
-  ]
+const MeshImages = [
+  './MD1.jpg',
+  './MD2.webp',
+  './MD3.jpg',
+  './MD4.jpg',
+  './MD5.jpeg',
+  './MD6.webp'
+]
 
-  const grillsImages = [
-    './grills1.webp',
-    './grills2.webp',
-    './grills3.webp',
-    './grills4.jpg',
-    './grills5.webp',
-    './grills6.webp'
-  ]
+const grillsImages = [
+  './grills1.webp',
+  './grills2.webp',
+  './grills3.webp',
+  './grills4.jpg',
+  './grills5.webp',
+  './grills6.webp'
+]
 
 
 
 const Gallery = () => {
 
   const ImgUrl = "https://cdn-ikpjmaj.nitrocdn.com/tpiOjaVkKiYlOPaZEGMJBhdGjCOUFngB/assets/images/optimized/rev-79ca70e/elelaurels.com/wp-content/uploads/2016/12/Balcony.jpg";
-
-
-
-  /*--- CLOTH HANGER PRODUCTS----*/
-
-  /*--------MESH DOOR PRODUCTS----------*/
-  
-  /*------INVISIBLE GRILLS PRODUCT------*/
-
-  /*-------ARTIFICIAL GRASS PRODUCT--------*/
-
-  /*--------pegion net product----------*/
+  const heading = "Gallery";
 
 
   return (
     <>
-    <Hero ImgUrl={ImgUrl} heading={heading}/>
+      <Hero ImgUrl={ImgUrl} heading={heading} />
 
-    {/*-----cloth hanger products-----*/}
+      {/*-----cloth hanger products-----*/}
 
-    <div className ='Gallery'>
-      
+      <div className='Gallery'>
+
         <h2><span>CLOTH</span> DRYING CEILING <span>HANGER </span> Products</h2>
         <div className='gallery-grid'>
-        <div className='Cloth Hangers'>
-          {images.map((src,index)=>(
-            <div key ={index} className='image-card'>
-            <img src = {src} alt={`clothhangers ${index+1}`} />
+          <div className='Cloth Hangers'>
+            {images.map((src, index) => (
+              <div key={index} className='image-card'>
+                <img src={src} alt={`clothhangers ${index + 1}`} />
+              </div>
+            ))}
+
+          </div>
+        </div>
+
+
+
+        { /*---mesh door products------*/}
+        <div className='Gallery'>
+          <div className='mesh-gallery'>
+            <h2>MOSQUITO <span>MESH DOOR</span>  PRODUCTS</h2>
+            <div className='gallery-grid'>
+              <div className='Mesh Door'>
+                {MeshImages.map((src, index) => (
+                  <div key={index} className='image-card'>
+                    <img src={src} alt={`MeshDoors ${index + 1}`} />
+                  </div>
+                ))}
+
+              </div>
             </div>
-          ))}
+
+          </div>
 
         </div>
       </div>
-     
+      {/*----Invisible product-------*/}
 
-
-      { /*---mesh door products------*/ }
-      <div className='Gallery'>
-      <div className ='mesh-gallery'>
-        <h2>MOSQUITO <span>MESH DOOR</span>  PRODUCTS</h2>
-        <div className='gallery-grid'>
-          <div className='Mesh Door'>
-             {MeshImages.map((src,index)=>(
-            <div key ={index} className='image-card'>
-            <img src = {src} alt={`MeshDoors ${index+1}`} />
-          </div>
-             ))}
-
-        </div>
-        </div>
-
- {/*    </div>
-      
-    </div>
-    </div> */}
-    {/*----Invisible product-------*/}
-
-             <div className='Grills-gallery'>
+      <div className='Grills-gallery'>
         <h2> <span>INVISIBLE GRILLS</span>  PRODUCTS</h2>
         <div className='gallery-grid'>
           <div className='Invisible grills'>
-             {grillsImages.map((src,index)=>(
-            <div key ={index} className='image-card'>
-            <img src = {src} alt={`Invisible grills ${index+1}`} />
+            {grillsImages.map((src, index) => (
+              <div key={index} className='image-card'>
+                <img src={src} alt={`Invisible grills ${index + 1}`} />
+              </div>
+            ))}
+
           </div>
-             ))}
-
-        </div>
         </div>
 
       </div>
-      </div>
-      </div>
-      </div>
-      
-    
+  
 
-    <RawMaterial />
+
+
+  <RawMaterial />
     </>
   )
 }
