37d07f3a9b0eec95b4edf2184bec884fa50e3ec5
vyshnavi <vishuchowdary20@gmail.com>
2025-07-09 13:39:01 +0530

    Product1 created

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,17 +1,64 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import RawMaterial from '../components/RawMaterial.jsx';
+import '../css/Gallery.css';
 
-const Gallery = () => {
-  const ImgUrl = "https://cdn-ikpjmaj.nitrocdn.com/tpiOjaVkKiYlOPaZEGMJBhdGjCOUFngB/assets/images/optimized/rev-79ca70e/elelaurels.com/wp-content/uploads/2016/12/Balcony.jpg";
+
 
   const heading = "Gallery";
+  const images = [
+        './CH1.jpg',
+        './CH2.webp',
+        './CH3.jpg',
+        './CH4.webp',
+        './CH5.jpg',
+        './CH6.webp',
+        './CH7.jpg',
+        './CH8.webp'
+  ];
+
+
+
+const Gallery = () => {
+
+  const ImgUrl = "https://cdn-ikpjmaj.nitrocdn.com/tpiOjaVkKiYlOPaZEGMJBhdGjCOUFngB/assets/images/optimized/rev-79ca70e/elelaurels.com/wp-content/uploads/2016/12/Balcony.jpg";
+
+
+
+  /*--- CLOTH HANGER PRODUCTS----*/
+
+  /*--------MESH DOOR PRODUCTS----------*/
+  
+  /*------INVISIBLE GRILLS PRODUCT------*/
+
+  /*-------ARTIFICIAL GRASS PRODUCT--------*/
+
+  /*--------pegion net product----------*/
+
+
   return (
     <>
     <Hero ImgUrl={ImgUrl} heading={heading}/>
+
+    <div className ='Gallery'>
+      <div className='gallery-grid'>
+        <h1>Cloth Drying Ceiling Hanger Products</h1>
+        <div className='Cloth Hangers'>
+          {images.map((src,index)=>(
+            <div key ={index} className='image-card'>
+            <img src = {src} alt={`clothhangers ${index+1}`} />
+            </div>
+          ))}
+
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
     <RawMaterial />
     </>
   )
 }
 
-export default Gallery
+export default Gallery;
