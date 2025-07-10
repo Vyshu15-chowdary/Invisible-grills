206cfb9cd53bc59927cab084109bfea258c66d7c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-10 22:18:35 +0530

    QuickServiceRequest Form Validation Added.

--- src/components/QuickServiceRequest.jsx
+++ src/components/QuickServiceRequest.jsx
@@ -7,18 +7,30 @@ const QuickServiceRequest = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
 
-  const handleSubmit =  (e)=>{
-    e.preventDefault();
-    
-    const message = `Hi, I'm ${name} requesting for QUICK SERVICE. 
-    Please, contact me +91${phone}. 
-    Thank you.`
+
   
+
+  
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!name){
+      return;
+    }
+    if(!phone){
+      return;
+    }
+
+    const message = `Hi, I'm ${name} requesting for QUICK SERVICE. 
+    Please, Contact me +91${phone}. 
+
+    Thank you.`
+
     const encodeMessage = encodeURIComponent(message);
     const phoneNumber = "919052585999";
-
     const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeMessage}`;
-
     window.open(whatsappURL, '_blank');
   }
 
@@ -29,16 +41,16 @@ const QuickServiceRequest = () => {
         <h3>Quick Service Request</h3>
         <h1>Free Inspection Request</h1>
       </div>
-     
-        <form onSubmit={handleSubmit} >
-          
-            <input autoComplete='off' type="text" value={name} onChange={(e)=>setName(e.target.value)} name='name' placeholder='Enter Full Name' />
-            <input autoComplete='off' type="text" name='phone' value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder='Enter Phone Number' />
-       
-            <button type='submit'>Send</button>
-        
-        </form>
-     
+
+      <form onSubmit={handleSubmit} >
+
+        <input  autoComplete='off' type="text" value={name} onChange={(e) =>setName(e.target.value)} name='name' placeholder='Enter Full Name' />
+        <input autoComplete='off' type="text" name='phone' value={phone} onChange={(e) => setPhone(e.target.value)} placeholder='Enter Phone Number' />
+
+        <button type='submit'>Send</button>
+
+      </form>
+
     </div>
   )
 }
