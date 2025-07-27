5b64831ba79801fea9813952e747b9af3b5e7a8b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-27 12:47:03 +0530

    form ui ready.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import "../css/contactForm.css"
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
+    <div className='cf-container'>
+      <form action="">
+        <h1>Write Message Here</h1>
+     
+        <br />
+
+        <input type="text" placeholder='Enter Your Name' />
+        <input type="text" placeholder='Enter Phone Number' />
+        <textarea name="" id="" placeholder='Write Your Message' rows={5}></textarea>
+        <button>Send Message</button>
+      </form>
+
+
+
+    </div>
   )
 }
 
