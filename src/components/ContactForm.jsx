9c5f075ca33160ccc39db7fac781afe848cd7b96
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-27 11:18:41 +0530

    contact form component created.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,26 +1,9 @@
-import React, { useEffect } from 'react'
-import GoogleMap from '../components/GoogleMap'
-import Hero from '../components/Hero';
-import Achievements from "../components/Achievements.jsx"
-import ContactForm from '../components/ContactForm.jsx';
+import React from 'react'
 
-const Contact = () => {
-  const ImgUrl = "https://hangerswala.com/wp-content/uploads/2025/05/assets_task_01jv1v3z1dfjs8c6zszwdbcdsh_1747040025_img_0.webp";
-  const heading = "Contact Us";
-
-  useEffect(()=>{
-    document.title = "Contact Us"
-
-  }, [])
+const ContactForm = () => {
   return (
-    <>
-    <Hero ImgUrl={ImgUrl} heading={heading}/>
- 
-    <ContactForm/>
-    <Achievements/>
-    <GoogleMap/>
-    </>
+    <div>ContactForm</div>
   )
 }
 
-export default Contact
+export default ContactForm
