d3590864dab105945984a53b3425494c10010733
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-27 13:31:19 +0530

    form functionality added.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,16 +5,38 @@ const ContactForm = () => {
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
 
+
+
+  const phoneNumber = '919052585999';
+
+  const finalMessage = `
+    ${message}
+
+    with warm regards,
+    - ${name}
+    - ${phone}.
+    `;
+
+  const encodedMessage = encodeURIComponent(finalMessage);
+  // handle submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappURL, '_blank');
+  }
+
   return (
     <div className='cf-container'>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <h1>Write Message Here</h1>
-     
+
         <br />
 
-        <input type="text" value={name}  onChange={(e)=>setName(e.target.value)}  placeholder='Enter Your Name' />
-        <input type="text" placeholder='Enter Phone Number' value={phone} onChange={(e)=>setPhone(e.target.value)} />
-        <textarea name="" id="" placeholder='Write Your Message' value={message} onChange={(e)=>setMessage(e.target.value)} rows={5}></textarea>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder='Enter Your Name' />
+        <input type="text" placeholder='Enter Phone Number' value={phone} onChange={(e) => setPhone(e.target.value)} />
+        <textarea name="" id="" placeholder='Write Your Message' value={message} onChange={(e) => setMessage(e.target.value)} rows={5}></textarea>
         <button>Send Message</button>
       </form>
 
