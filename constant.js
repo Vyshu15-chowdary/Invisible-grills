344ca68bf8ce78db4777b00234ed9492bb315461
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-30 16:43:23 +0530

    constant.js updated.

--- constant.js
+++ constant.js
@@ -1,5 +1,8 @@
 export const FOUNDER_NAME = "Sasikanth Yendluri";
 export const FOUNDER_EMAIL = "sasikanth@example.com";
-export const FOUNDER_PHONE = "+91 12345 67890";
+export const FOUNDER_PHONE = "+91 6303 744627";
+export const FOUNDER_WHATSAPP = "+91 6303 744627";
 export const COMPANY_NAME = "Vyshnavi Invisible Grills";
 export const FOUNDER_IMAGE = "https://static.vecteezy.com/system/resources/thumbnails/048/926/084/small_2x/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-illustration-vector.jpg";
+
+export const COMPANY_ADDRESS = "B.v nagar sapthagiri colony Old Vedhayapalem Mini Bypass. Near Ayyapa gudi fly over Nellore - 524004"
