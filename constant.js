4f66592f640ef2364b00e970a755c9eb2b1bbd2f
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-30 16:32:13 +0530

    constant.js added.

---
+++ constant.js
@@ -0,0 +1,5 @@
+export const FOUNDER_NAME = "Sasikanth Yendluri";
+export const FOUNDER_EMAIL = "sasikanth@example.com";
+export const FOUNDER_PHONE = "+91 12345 67890";
+export const COMPANY_NAME = "Vyshnavi Invisible Grills";
+export const FOUNDER_IMAGE = "https://static.vecteezy.com/system/resources/thumbnails/048/926/084/small_2x/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-illustration-vector.jpg";
