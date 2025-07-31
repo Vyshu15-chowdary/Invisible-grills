fcb3c01220b1a1f3a99ba7efe5fc13da0e7ac955
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-07-31 14:28:20 +0530

    whatsapp number updated.

--- constant.js
+++ constant.js
@@ -1,7 +1,7 @@
 export const FOUNDER_NAME = "Sasikanth Yendluri";
 export const FOUNDER_EMAIL = "sasikanth@example.com";
 export const FOUNDER_PHONE = "+91 6303 744627";
-export const FOUNDER_WHATSAPP = "+91 6303 744627";
+export const FOUNDER_WHATSAPP = "919052585999";
 export const COMPANY_NAME = "Vyshnavi Invisible Grills";
 export const FOUNDER_IMAGE = "https://static.vecteezy.com/system/resources/thumbnails/048/926/084/small_2x/silver-membership-icon-default-avatar-profile-icon-membership-icon-social-media-user-image-illustration-vector.jpg";
 // export const COMPANY_LOGO = ;
