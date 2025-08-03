3d14f437787e40a40d988c71a096cce1f13a9166
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-03 17:44:33 +0530

    address updated.

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -6,7 +6,7 @@ const GoogleMap = () => {
     
   return (
     <div className='google-map-wrapper'>
-    <iframe className='google-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7728.788697920529!2d79.95027199672242!3d14.404416342142394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4cf2fc39c18115%3A0x8c14bb4eb08fde75!2sSapthagiri%20Colony%2C%20Simhapuri%20Center%2C%20Thayagaraju%20Nagar%2C%20Vedayapalem%2C%20Nellore%2C%20Andhra%20Pradesh%20524004!5e0!3m2!1sen!2sin!4v1754214989918!5m2!1sen!2sin" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    <iframe className='google-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3864.334851252054!2d79.95698317486647!3d14.407850581767105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4cf3bdda5c7af1%3A0x64443930295bee8f!2sOld%20vedayapalem%20Nellore!5e0!3m2!1sen!2sin!4v1754223145940!5m2!1sen!2sin" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
   )
 }
